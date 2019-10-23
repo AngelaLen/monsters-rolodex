87a4e5fcc1fb64ef8049279248c133bd802e9316
Angela Lengkeek <angelalengkeek@gmail.com>
2019-10-23 14:33:55 +0200

    Updates

---
+++ precache-manifest.4dd598cbe5dca6a1257b999d9751752f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "922082ad62a71d0bd8b49b02a7ed03f9",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1c4ef063f873227e3a3e",
+    "url": "/monsters-rolodex/static/css/main.f30109a3.chunk.css"
+  },
+  {
+    "revision": "f977fa80ff73af86627e",
+    "url": "/monsters-rolodex/static/js/2.37e904de.chunk.js"
+  },
+  {
+    "revision": "1c4ef063f873227e3a3e",
+    "url": "/monsters-rolodex/static/js/main.9f30ae2a.chunk.js"
+  },
+  {
+    "revision": "f56f2e2c6d96b014d91f",
+    "url": "/monsters-rolodex/static/js/runtime-main.86190389.js"
+  }
+]);
